70ef943f74ab7a7f3659353373590976facb63ac
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-03-16 13:21:07 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-puts("Programming is like building a multilingual puzzle)
+#include <stdio.h> 
+  
+ /** 
+   * main - Main function 
+   * 
+   * Return: Always 0 (Success) 
+   */ 
+ int main(void) 
+ { 
+         puts("\"Programming is like building a multilingual puzzle"); 
+  
+         return (0); 
+ }
