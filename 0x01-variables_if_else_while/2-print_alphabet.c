41a9c5cf18b4804940b942f8b55f2f68a83ddbac
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-03-17 16:16:16 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	char i;
+	char x;
 
-	for (i = 'a' ; i <= 'z' ; i++)
-		putchar(i + '0');
-	putchar("\n");
+	for (x = 'a' ; x <= 'z' ; x++)
+		putchar(x);
+	putchar('\n');
 	return (0);
 }
