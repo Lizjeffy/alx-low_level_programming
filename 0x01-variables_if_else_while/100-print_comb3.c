76c23293ddc557b59131781fd9dcb24d015f3a45
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-03-17 19:26:15 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0 ; x < 9 ; x++)
+	for (y = 1 ; y < 10 ; y++)
+	{
+		if (y > x && y != x)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+		}
+		if (x + y < 17)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
