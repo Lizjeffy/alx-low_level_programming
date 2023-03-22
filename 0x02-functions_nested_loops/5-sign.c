ff7492b20df060216f483700a5986bcba4cfc9c3
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-03-22 05:04:33 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - function to print the sign of a number
+ *
+ * @n: function parameter
+ *
+ * Return: 1 if the sign of a number is positive, 0 if it is 0 and -1 otherwise
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
