48f82f94ab9e299730b20fec514903e53942b424
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-03-22 03:58:17 -0700

    Check for c alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - function to check for alphabet c
+ *
+ * @c: parametr to be checked
+ *
+ * Return: 1 if c is an alphabet and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 7 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
