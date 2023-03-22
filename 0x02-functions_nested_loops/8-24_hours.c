7a35e8e3c37256c5cf7c2320ded4235453d0f64c
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-03-22 08:04:52 -0700

    Print time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function to print time
+ *
+ * Return: always 0
+ */
+
+void jack_bauer(void)
+{
+	int x;
+	int y;
+
+	for (x = 0 ; x < 24 ; x++)
+	{
+		for (y = 0 ; y < 60 ; y++)
+		{
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			_putchar(':');
+			_putchar(y /10 + '0');
+			_putchar(y % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
