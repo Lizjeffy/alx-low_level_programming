2bdcd274c8b20ac48dda283ddd4f3f51f9207345
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-03-22 13:45:56 -0700

    Print thr first 98 Fibonacci nums

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,9 +11,9 @@ int main(void)
 	int x = 0;
 	long y = 1, z = 2;
 
-	while (x < 50)
+	while (x < 98)
 	{
-		if (x ==0)
+		if (x == 0)
 			printf("%ld", y);
 		else if (x == 1)
 			printf(", %ld", z);
