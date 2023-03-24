1d249aa1ad087669ff8b817857fbfa169adfe700
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-03-24 02:21:34 -0700

    multiple of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function to multiple two integrs
+ * @a: integer 1
+ * @b: integer 2
+ * Return: c
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
