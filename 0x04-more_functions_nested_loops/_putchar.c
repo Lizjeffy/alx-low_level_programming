a5efb9f6ed685e5cabc78e5f0ca51521f4a7be5f
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-03-24 03:01:52 -0700

    Prints all digits

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - function to write the character c to stdout
+ * @c: character to be printed
+ * Return: 1
+ * on error, -1 is required, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
