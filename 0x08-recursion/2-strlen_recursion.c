10209697c15c8b598385241d221d4a9fc0ef71bd
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-04-05 03:57:05 -0700

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * Return: a
+ */
+
+int _strlen_recursion(char *s)
+{
+	int a = 0;
+
+	if (*s == '\0')
+		return(0);
+	a = 1 + _strlen_recursion(s + 1);
+	return(a);
+}
