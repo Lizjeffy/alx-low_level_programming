38df3c14c7bdfb5723f12da409808ce8f1771f5f
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-04-05 03:43:40 -0700

    Print reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,12 +7,8 @@
 
 void _print_rev_recursion(char *s)
 {
-
 	if (*s == '\0')
-		_putchar('\n');
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(--s);
-	}
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
