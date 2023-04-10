a47d0d85523d740676fa7ba56692a47614953413
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-04-10 07:03:44 -0700

    Multiple of two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int product;
+	int x;
+
+	for (x = 1; x < argc; x++)
+
+	product = atoi(argv[1]) * atoi(argv[2]);
+
+	{
+		if (argc < 3 || argc > 3)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", product);
+	return (0);
+}
