4634ba69f688167d5d207f12dc0290982fa4a089
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-04-10 15:39:56 -0700

    minimal number

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,20 +14,18 @@ int main(int argc, char *argv[])
 {
 	int cents;
 	int num_coins;
+
 	cents = atoi(argv[1]);
 	num_coins = 0;
-
 	if (argc != 2)
 	{
 		printf("Error\n");
 	}
-
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-
 	while (cents >= 25)
 	{
 		cents -= 25;
@@ -38,13 +36,11 @@ int main(int argc, char *argv[])
 		cents -= 10;
 		num_coins++;
 	}
-
 	while (cents >= 5)
 	{
 		cents -= 5;
 		num_coins++;
 	}
-
 	while (cents >= 2)
 	{
 		cents -= 2;
